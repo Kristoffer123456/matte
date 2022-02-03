4d7239187d61bccfc6bbddf1ebcd365897c04485
Kristoffer123456 <98930162+Kristoffer123456@users.noreply.github.com>
2022-02-03 03:22:37 +0100

    Add files via upload

---
+++ Math for kids/Scripts/multiply.js
@@ -0,0 +1,79 @@
+const option1 = document.getElementById ("option1")
+const option2 = document.getElementById ("option2")
+const option3 = document.getElementById ("option3")
+const wrongAudio = document.getElementById ("wrong-audio")
+const successAudio = document.getElementById ("success-audio")
+const correctCard = document.getElementById ("correct")
+var answer = 0
+
+function generate_equation (){
+    var num1 = Math.floor(Math.random() * 10)
+    var num2 = Math.floor(Math.random() * 10)
+    var dummyAnswer1 = Math.floor(Math.random() * 50)
+    var dummyAnswer2 = Math.floor(Math.random() * 50)
+    var allAnswers = []
+    var switchAnswers = []
+
+    answer = eval(num1 * num2)
+
+    document.getElementById("num1").innerHTML = num1
+    document.getElementById("num2").innerHTML = num2
+
+    if (dummyAnswer1 === answer || dummyAnswer2 === answer || dummyAnswer1 === dummyAnswer2){
+        var dummyAnswer1 = Math.floor(Math.random() * 13)
+        var dummyAnswer2 = Math.floor(Math.random() * 13)
+    }
+
+    allAnswers = [answer, dummyAnswer1, dummyAnswer2]
+
+    for (i = allAnswers.length; i--;) {
+        switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0])
+    }
+
+    option1.innerHTML = switchAnswers[0]
+    option2.innerHTML = switchAnswers[1]
+    option3.innerHTML = switchAnswers[2]
+}
+
+
+option1.addEventListener ("click", function(){
+    if(option1.innerHTML == answer) {
+        generate_equation()
+        successAudio.play()
+        correctCard.style.display = ("flex")
+        setTimeout (function(){
+            correctCard.style.display = ("none")
+        },2000)
+    } else {
+        wrongAudio.play()
+    }
+})
+
+option2.addEventListener ("click", function(){
+    if(option2.innerHTML == answer) {
+        generate_equation()
+        successAudio.play()
+        correctCard.style.display = ("flex")
+        setTimeout (function(){
+            correctCard.style.display = ("none")
+        },2000)
+    } else {
+        wrongAudio.play()
+    }
+})
+
+option3.addEventListener ("click", function(){
+    if(option3.innerHTML == answer) {
+        generate_equation()
+        successAudio.play()
+        correctCard.style.display = ("flex")
+        setTimeout (function(){
+            correctCard.style.display = ("none")
+        },2000)
+    } else {
+        wrongAudio.play()
+    }
+})
+
+generate_equation ()
+
